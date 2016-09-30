13deec1c25931f17ca8b5b39031597665e9eaa67
parthibanrsnit <parthiban.s@rsninfotech.com>
2016-09-30 12:40:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin_2/myapplication2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.admin_2.myapplication2;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i1);
+            }
+        });
+    }
+}
