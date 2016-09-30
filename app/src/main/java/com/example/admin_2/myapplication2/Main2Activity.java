7116e6ee0e5a75f82cd80e410f28f6aba1a5be4d
parthibanrsnit <parthiban.s@rsninfotech.com>
2016-09-30 12:43:05 +0530

    Commited Files Nows

--- app/src/main/java/com/example/admin_2/myapplication2/Main2Activity.java
+++ app/src/main/java/com/example/admin_2/myapplication2/Main2Activity.java
@@ -15,7 +15,7 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        //ksjyij
+        //ksjyij cvcvccccxvcxv
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
